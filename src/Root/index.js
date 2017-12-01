17e609ee698d35817c7cc241d0b3141901c69bab
jrmeunrath <jrmeun104@gmail.com>
2017-11-30 19:56:02 -0700

    added client side routing

--- src/Root/index.js
+++ src/Root/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import {
-  BrowserRouter,
   Route,
-  Switch
+  NavLink,
+  HashRouter
 } from 'react-router-dom';
 import Layout from '../Layout';
 import routes from '../routes';
 
 const Root = () => (
-  <div>
+  <HashRouter>
     <div>
-      <h1>Header</h1>
-      <ul>
-        <li><a href='/'>Home</a></li>
-        <li><a href='/about'>About</a></li>
-      </ul>
+      <div>
+        <h1>Header</h1>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+        </ul>
+      </div>
+        <Layout>
+          {routes.map((route, index) => <Route key={`${route.path}-${index}`} {...route} />)}
+        </Layout>
+      <div>
+        <h4>Footer</h4>
+      </div>
     </div>
-    <Layout>
-      <BrowserRouter>
-        <Switch>
-          { routes.map((route, i) => <Route key={i} {...route} />) }
-        </Switch>
-      </BrowserRouter>
-    </Layout>
-    <div>
-      <h4>Footer</h4>
-    </div>
-  </div>
+  </HashRouter>
 );
 
 export default Root;
