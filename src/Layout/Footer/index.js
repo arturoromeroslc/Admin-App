a2392270274be269706459edffb108673dc9ab5a
JR Meunrath <jrmeun104@gmail.com>
2018-04-26 13:03:37 -0600

    connect Footer component to redux

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -1,50 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import {
+    onLoadFooterLinks
+} from './actions';
 import './styles/index.less';
 
-export default function Footer() {
-  return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="footer-navigation">
-          <ul className="footer-menu">
-            <li className="menu-item">
-              <h4>Links</h4>
-              <ul className="sub-menu">
-                <li className="menu-item">github.com</li>
-                <li className="menu-item">linkedin.com</li>
+class Footer extends Component {
+  componentDidMount() {
+    this.props.onLoadFooterLinks();
+  }
+
+  render() {
+      return (
+        <div className="main-footer">
+          <div className="container">
+            <div className="footer-navigation">
+              <ul className="footer-menu">
+                <li className="menu-item">
+                  <h4>Links</h4>
+                  <ul className="sub-menu">
+                    <li className="menu-item">github.com</li>
+                    <li className="menu-item">linkedin.com</li>
+                  </ul>
+                </li>
+                <li className="menu-item">
+                  <h4>Sponsors</h4>
+                  <ul className="sub-menu">
+                    <li className="menu-item">github.com</li>
+                    <li className="menu-item">linkedin.com</li>
+                  </ul>
+                </li>
+                <li className="menu-item">
+                  <h4>Affiliates</h4>
+                  <ul className="sub-menu">
+                    <li className="menu-item">github.com</li>
+                    <li className="menu-item">linkedin.com</li>
+                  </ul>
+                </li>
+                <li className="menu-item">
+                  <h4>My Account</h4>
+                  <ul className="sub-menu">
+                    <li className="menu-item">github.com</li>
+                    <li className="menu-item">linkedin.com</li>
+                  </ul>
+                </li>
+                <li className="menu-item">
+                  <h4>Resources</h4>
+                  <ul className="sub-menu">
+                    <li className="menu-item">github.com</li>
+                    <li className="menu-item">linkedin.com</li>
+                  </ul>
+                </li>
               </ul>
-            </li>
-            <li className="menu-item">
-              <h4>Sponsors</h4>
-              <ul className="sub-menu">
-                <li className="menu-item">github.com</li>
-                <li className="menu-item">linkedin.com</li>
-              </ul>
-            </li>
-            <li className="menu-item">
-              <h4>Affiliates</h4>
-              <ul className="sub-menu">
-                <li className="menu-item">github.com</li>
-                <li className="menu-item">linkedin.com</li>
-              </ul>
-            </li>
-            <li className="menu-item">
-              <h4>My Account</h4>
-              <ul className="sub-menu">
-                <li className="menu-item">github.com</li>
-                <li className="menu-item">linkedin.com</li>
-              </ul>
-            </li>
-            <li className="menu-item">
-              <h4>Resources</h4>
-              <ul className="sub-menu">
-                <li className="menu-item">github.com</li>
-                <li className="menu-item">linkedin.com</li>
-              </ul>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+  }
+};
+
+Footer.propTypes = {
+  onLoadFooterLinks: PropTypes.func.isRequired
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    onLoadFooterLinks
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(Footer);
